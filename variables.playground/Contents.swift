5bc71dfc345af3553a01b5082d628bbf84008b13
Adnan Saleem <80086557+Adnan-Saleem-93@users.noreply.github.com>
2026-01-27 16:56:55 +0500

    updated "string" variables section

--- variables.playground/Contents.swift
+++ variables.playground/Contents.swift
@@ -1,5 +1,11 @@
 import Foundation
 
+
+// string
 let greeting = "Hello, playground"
 print(greeting.uppercased().hasSuffix("ND"))
+print(greeting.count)
 
+// string interpolation
+let name = "Adnan"
+print("My name is \(name)")
