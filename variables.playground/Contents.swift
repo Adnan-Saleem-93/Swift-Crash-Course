2df475fc10592cfeb7d2a1719480672f2b8b8718
Adnan Saleem <80086557+Adnan-Saleem-93@users.noreply.github.com>
2026-01-27 17:04:03 +0500

    learned about number variables

--- variables.playground/Contents.swift
+++ variables.playground/Contents.swift
@@ -9,3 +9,14 @@ print(greeting.count)
 // string interpolation
 let name = "Adnan"
 print("My name is \(name)")
+
+// numbers
+let num1 = 10
+let num2 = 15
+var sum = num1 + num2
+print(sum)
+sum += 5
+print("sum after adding 5 more = \(sum)")
+let multipleOf = 5
+print("sum is multiple of \(multipleOf): \(sum.isMultiple(of: multipleOf))")
+print("random number between 1 and 1000 -> \(Int.random(in: 1...1000))")
