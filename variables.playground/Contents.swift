81355d686e9ad38edc2287897b2c48a1675379ab
Adnan Saleem <80086557+Adnan-Saleem-93@users.noreply.github.com>
2026-01-29 22:35:00 +0500

    learned about if and switch statements

--- variables.playground/Contents.swift
+++ variables.playground/Contents.swift
@@ -78,3 +78,40 @@ enum Themes {
 	case light, dark, system
 }
 var UI: Themes = Themes.dark
+
+// if-else conditions
+let age = 30
+if age > 40 {
+	print("You are over the age limit.")
+}else if age < 30 {
+	print("You are under the age limit.")
+}else {
+	print("You qualify.")
+}
+
+let price = 400
+if price >= 400 && price <= 500 {
+	print("The price is justified.")
+}else if price > 300 || price < 400 {
+	print("The price is on sale.")
+}else {
+	print("The price is over-priced.")
+}
+
+// switch statement
+enum WeatherCondition {
+	case sunny, rainy, windy, overcast
+}
+
+var currentWeather = WeatherCondition.overcast
+
+switch currentWeather {
+case .rainy:
+	print("It is raining today.")
+case .sunny:
+	print("It is sunny outside.")
+case .windy:
+	print("It is cold outside.")
+default:
+	print("The weather is unusual.")
+}
