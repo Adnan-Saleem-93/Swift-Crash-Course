b6b82ee75dea308575eff24a632089cdef00f514
Adnan Saleem <80086557+Adnan-Saleem-93@users.noreply.github.com>
2026-01-30 16:31:00 +0500

    learned about ternary operator and loops

--- variables.playground/Contents.swift
+++ variables.playground/Contents.swift
@@ -115,3 +115,30 @@ case .windy:
 default:
 	print("The weather is unusual.")
 }
+
+// ternary operation
+let people = 100
+let isFull = people >= 100 ? "Capacity Full." : "Still room for more."
+print(isFull)
+
+// loops
+let filesTypes = ["jpeg","png","jpg","txt","doc"]
+/// for loops
+for type in filesTypes {
+	if type == "txt" || type == "doc" {
+		continue
+	}
+	print("File type \(type.uppercased()) is supported")
+}
+for i in 1..<11 {
+	print("5 x \(i) = \(5*i)")
+}
+
+/// while loop
+print("\nStarting countdown")
+var num = 10
+while num>0 {
+	print("\(num)...")
+	num -= 1
+}
+print("Done")
