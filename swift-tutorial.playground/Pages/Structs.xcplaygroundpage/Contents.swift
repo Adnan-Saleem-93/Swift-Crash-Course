7948c3f9e701c56c27b1beff9b9ede5e65e57220
Adnan Saleem <80086557+Adnan-Saleem-93@users.noreply.github.com>
2026-02-04 16:04:42 +0500

    learned about structs

--- swift-tutorial.playground/Pages/Structs.xcplaygroundpage/Contents.swift
+++ swift-tutorial.playground/Pages/Structs.xcplaygroundpage/Contents.swift
@@ -2,6 +2,30 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+struct Book {
+	let author:String
+	let title: String
+	let releaseYear: Int
+	var edition: Int = 1
+	var ratings: Double  = 0
+	
+	func getDetails(){
+		print("\"\(title)\" by \(author), released in \(releaseYear), currently on its \(edition).")
+	}
+	func getRatings(){
+		print("The book has a rating of \(ratings)")
+	}
+	
+	mutating func updateRatings(_ newRatings : Double){
+		ratings = newRatings
+	}
+}
+
+var book = Book(author: "George R. R. Martin", title: "A Song of Ice and Fire", releaseYear: 2006)
+
+book.getDetails()
+book.getRatings()
+book.updateRatings(4.3)
+book.getRatings()
 
 //: [Next](@next)
